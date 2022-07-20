0b4aeffe2d1d5a1d5efa98778740f5d2dc404437
platojobs <platojobs@qq.com>
2022-07-20 18:37:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from lxml.etree import CDATA
 
 MD_HEAD = """
 ## Gitblog  My personal blog using issues and GitHub Actions
+
 ![](https://github.com/platojobs/platojobs/blob/main/assets/home.gif)
+
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
 
